4470af2e853bf7c0bcae6824221eb50fdd2c62d7
liangtianci <1959574493@qq.com>
2025-09-01 00:46:08 +0800

    高亮标题栏当前页面按钮

--- src/renderer/src/components/Title.vue
+++ src/renderer/src/components/Title.vue
@@ -1,7 +1,7 @@
 <template>
     <header id="app-title"
         class="drag h-[32px] flex justify-between box-content bg-[#f0f4f9] border-b-[1px] border-b-[rgba(0,0,0,0.1)]">
-        <section class="left flex items-center h-full no-drag cursor-pointer" @click="$router.push('/')">
+        <section class="left flex items-center h-full no-drag cursor-pointer" @click="toRouter('/')">
             <div class="pl-2 pr-1.5 h-full flex items-center">
                 <img class="max-h-[70%]" src="../assets/image/icon24x24.png" alt="">
             </div>
@@ -18,16 +18,16 @@
                     </el-button>
                 </my-tooltip>
                 <my-tooltip content="光标效果" placement="bottom">
-                    <el-button @click="$router.push('/cursor')" color="#f0f4f9"><i-solar-cursor-linear /></el-button>
+                    <el-button @click="toRouter('/cursor')" color="#f0f4f9" :class="{ active: isActive('/cursor')}"><i-solar-cursor-linear /></el-button>
                 </my-tooltip>
                 <my-tooltip content="我的收藏" placement="bottom">
-                    <el-button @click="$router.push('/favorites')" color="#f0f4f9"><i-ms-kid-star-outline /></el-button>
+                    <el-button @click="toRouter('/favorites')" color="#f0f4f9" :class="{ active: isActive('/favorites')}"><i-ms-kid-star-outline /></el-button>
                 </my-tooltip>
                 <my-tooltip content="我的壁纸" placement="bottom">
-                    <el-button @click="$router.push('/local')" color="#f0f4f9"><i-ep-folder-opened /></el-button>
+                    <el-button @click="toRouter('/local')" color="#f0f4f9":class="{ active: isActive('/local')}"><i-ep-folder-opened /></el-button>
                 </my-tooltip>
                 <my-tooltip content="设置" placement="bottom">
-                    <el-button @click="$router.push('/setting')" color="#f0f4f9"><i-ep-setting/></el-button>
+                    <el-button @click="toRouter('/setting')" color="#f0f4f9" :class="{ active: isActive('/setting')}"><i-ep-setting/></el-button>
                 </my-tooltip>
                 <div class="line absolute w-[0.5px] h-[50%] bg-[rgba(0,0,0,0.1)] right-[-10px] top-1/2 translate-[-50%]">
                 </div>
@@ -46,8 +46,27 @@
 
 <script setup>
 import useUpdater from '@renderer/composables/useUpdater'
+import { useRoute, useRouter } from 'vue-router'
+import { ref } from 'vue'
 
 const { updateStatus, findUpdate, tipText, updateChange, cancelUpdate } = useUpdater()
+
+const route = useRoute()
+const router = useRouter()
+
+const currentRoute = ref(route.path)
+
+const toRouter = (path) => {
+    if(route.path !== path){
+        router.push(path)
+        currentRoute.value = path
+    }
+}
+
+const isActive = (path) => {
+    return route.path === path
+}
+
 
 //最小化App
 const minimizeApp = () => window.electronAPI.minimize()
@@ -61,6 +80,11 @@ const quitApp = () => window.electronAPI.quit()
     animation: bounce 1s 1s infinite;
 }
 
+:deep(.el-button.active) {
+    background-color: #d2dae2;
+    border-color: #d2dae2;
+    z-index:auto;
+}
 @keyframes bounce {
     0%, 20%, 53%, 100% {
         -webkit-animation-timing-function: cubic-bezier(0.215, 0.61, 0.355, 1);
